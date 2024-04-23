30b50ad14f7adc00b9ebebfee5b8501961a26427
Derek <DerekRoseRod@gmail.com>
2024-04-23 13:29:05 -0500

    Update

--- src/main/java/co/edu/uniquindio/poo/UsuarioEncargado.java
+++ src/main/java/co/edu/uniquindio/poo/UsuarioEncargado.java
@@ -7,8 +7,8 @@ public class UsuarioEncargado {
     public static int TARIFA_CARRO = 10000;       
 
     public static void setTarifas(int tarifaMotoClasica, int tarifaMotoHibrida, int tarifaCarro) {
-        TARIFA_MOTO_CLASICA = tarifaMotoClasica;
-        TARIFA_MOTO_HIBRIDA = tarifaMotoHibrida;
-        TARIFA_CARRO = tarifaCarro;
+        tarifaMotoClasica = TARIFA_MOTO_CLASICA;
+        tarifaMotoHibrida = TARIFA_MOTO_HIBRIDA;
+        tarifaCarro = TARIFA_CARRO;
     }
 }
