5ef09201f41e0f604793a0a0e1e1daa3b477a191
Derek <DerekRoseRod@gmail.com>
2024-04-22 22:58:02 -0500

    Update

--- src/main/java/co/edu/uniquindio/poo/PreciosTarifa.java
+++ src/main/java/co/edu/uniquindio/poo/PreciosTarifa.java
@@ -1,5 +1,29 @@
 package co.edu.uniquindio.poo;
 
 public class PreciosTarifa {
-    
+
+    private int MotoClasica;
+    private int MotoHibrida;
+    private int Carro;
+
+    public PreciosTarifa(){
+        this.MotoClasica = 2000;
+        this.MotoHibrida = 5000;
+        this.Carro = 10000;
+    }
+
+    public int CostoMotoClasica(){
+        int horaMotoClasica = 2 * MotoClasica;
+        return horaMotoClasica;
+    }
+
+    public int CostoMotoHibrida(){
+        int horaMotoHibrida = 2 * MotoHibrida;
+        return horaMotoHibrida;
+    }
+
+    public int CostoCarro(){
+        int horaCarro = 2 * Carro;
+        return horaCarro;
+    }
 }
