a63764e30a371231176e76d7622ec52a2cbabf79
Derek <DerekRoseRod@gmail.com>
2024-04-23 13:07:49 -0500

    Update

--- src/main/java/co/edu/uniquindio/poo/PreciosTarifa.java
+++ src/main/java/co/edu/uniquindio/poo/PreciosTarifa.java
@@ -1,29 +1,38 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Scanner;
+
 public class PreciosTarifa {
 
-    private int MotoClasica;
-    private int MotoHibrida;
-    private int Carro;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public PreciosTarifa(){
-        this.MotoClasica = 2000;
-        this.MotoHibrida = 5000;
-        this.Carro = 10000;
-    }
+        System.out.println("Ingrese el tipo de vehículo (1: Moto Clásica, 2: Moto Híbrida, 3: Carro):");
+        int tipoVehiculo = scanner.nextInt();
 
-    public int CostoMotoClasica(){
-        int horaMotoClasica = 2 * MotoClasica;
-        return horaMotoClasica;
-    }
+        System.out.println("Ingrese el número de horas de estacionamiento:");
+        int horas = scanner.nextInt();
 
-    public int CostoMotoHibrida(){
-        int horaMotoHibrida = 2 * MotoHibrida;
-        return horaMotoHibrida;
-    }
+        int costo = 0;
+        switch (tipoVehiculo) {
+            case 1: 
+                costo = horas * UsuarioEncargado.TARIFA_MOTO_CLASICA;
+                break;
+            case 2: 
+                costo = horas * UsuarioEncargado.TARIFA_MOTO_HIBRIDA;
+                break;
+            case 3: 
+                costo = horas * UsuarioEncargado.TARIFA_CARRO;
+                break;
+            default:
+                System.out.println("Tipo de vehículo no válido.");
+                break;
+        }
 
-    public int CostoCarro(){
-        int horaCarro = 2 * Carro;
-        return horaCarro;
+        if (costo > 0) {
+            System.out.println("El costo total del estacionamiento es: " + costo);
+        }
+
+        scanner.close();
     }
 }
--- src/main/java/co/edu/uniquindio/poo/UsuarioEncargado.java
+++ src/main/java/co/edu/uniquindio/poo/UsuarioEncargado.java
@@ -2,4 +2,13 @@ package co.edu.uniquindio.poo;
 
 public class UsuarioEncargado {
     
+    public static int TARIFA_MOTO_CLASICA = 1000; 
+    public static int TARIFA_MOTO_HIBRIDA = 5000; 
+    public static int TARIFA_CARRO = 10000;       
+
+    public static void setTarifas(int tarifaMotoClasica, int tarifaMotoHibrida, int tarifaCarro) {
+        TARIFA_MOTO_CLASICA = tarifaMotoClasica;
+        TARIFA_MOTO_HIBRIDA = tarifaMotoHibrida;
+        TARIFA_CARRO = tarifaCarro;
+    }
 }
